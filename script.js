2e77505f7cb162adf9e9b5fed5e009626f5b56b7
sohel rana <sohel0130844@gmail.com>
2024-11-28 00:12:56 +0600

    prothom update

---
+++ script.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector('.nav_links');
+let xmark = document.querySelector('.xmark');
+let bars = document.querySelector('.bars')
+let Head_nav = document.querySelector('#Head_nav')
+
+function myclas() {
+    navbar.classList.toggle('activeNav');
+}
+
+function closeFunction() {
+    navbar.classList.remove('activeNav');
+}
+
+function forNavf() {
+    navbar.classList.remove('activeNav');
+}
+window.addEventListener('scroll', function () {
+    navbar.classList.remove('activeNav', window.scrollY > 0)
+    Head_nav.classList.toggle('scrollxy', window.scrollY > 0)
+})
